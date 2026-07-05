d599c592adf3f1f27914885d210a7a47ab913fcb
Allen Kizito Wachio <allenkizitowachio@gmail.com>
2026-07-05 14:46:21 +0300

    Fixed bug caused my mislabelling

--- src/object_task/config.py
+++ src/object_task/config.py
@@ -8,4 +8,4 @@ OBJECT_CONF_THRESHOLD = float(os.environ.get("OBJECT_CONF_THRESHOLD", "0.35"))
 OBJECT_IOU_THRESHOLD = float(os.environ.get("OBJECT_IOU_THRESHOLD", "0.45"))
 OBJECT_INPUT_SIZE = int(os.environ.get("OBJECT_INPUT_SIZE", "640"))
 OBJECT_ENABLED = os.environ.get("OBJECT_ENABLED", "1") != "0"
-OBJECT_LABELS = ["Hammer", "Traffic Cone", "Tennis Ball"]
+OBJECT_LABELS = ["Traffic Cone", "Hammer", "Tennis Ball"]
